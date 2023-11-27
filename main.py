dd70194cd4509e0b7a4d758c81a5459aadf5d05a
Srijan-D <srijandubey3105@gmail.com>
2023-11-28 05:02:06 +0530

    Setting up the ui using bootstrap

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("index.html")
+
+
+app.run(debug=True)
+# this debug=True is used to run the server in debug mode
